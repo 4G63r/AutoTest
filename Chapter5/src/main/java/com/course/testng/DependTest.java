8cf3f3771513080ffca910bae6fd9a201958e2b3
songxiao <songxiao@100tal.com>
2018-12-18 16:12:19 +0800

    创建测试用例

---
+++ Chapter5/src/main/java/com/course/testng/DependTest.java
@@ -0,0 +1,14 @@
+package com.course.testng;
+
+import org.testng.annotations.Test;
+
+public class DependTest {
+    @Test
+    public void test1() {
+        System.out.println("test1 run");
+    }
+
+    public void test2() {
+        System.out.println("test2 run");
+    }
+}
